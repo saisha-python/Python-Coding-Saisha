12d3dda363fbb1a4feac035d086bd8358c05e195
saisha-python <saishasharma296@gmail.com>
2025-01-20 18:53:51 -0500

    Add files via upload

---
+++ If-then after class project.py
@@ -0,0 +1,8 @@
+print("Welcome. I am The Great Saisha. I shall tell you whether you have made good life choices.")
+a=int(input("Type either number 1 or 2. 1 if you like books and animals, or 2 if you do not like books or animals:"))
+b=1
+c=2
+if(a<2):
+    print("The Great Saisha says that you have made good life choices.")
+if(a>1):
+    print("The Great Saisha says that you have made very poor life choices.")
