94aa070e23dea0b28bbea985922e96c10c3b4df9
saisha-python <saishasharma296@gmail.com>
2025-01-25 11:20:46 -0500

    Add files via upload

---
+++ reverse a string.py
@@ -0,0 +1,7 @@
+string=input("Please enter your own string:")
+string2=('')
+for i in string:
+    string2=i+string2
+print("\n The Original String=", string)
+print("Great and all, whatever it is, The Reversed String=", string2)
+print("Words are awesome! :)")
