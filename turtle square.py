c5f6d5856a2f6fba3ba0859c747fd565c2e49032
Saisha Sharma <saishasharma296@gmail.com>
2025-02-06 17:55:51 -0500

    Add files via upload

---
+++ turtle square.py
@@ -0,0 +1,8 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor("powder blue")
+square_turtle = turtle.Turtle()
+for _ in range(4):
+    square_turtle.forward(100)
+    square_turtle.right(90) 
+turtle.done()
