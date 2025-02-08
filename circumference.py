bdeceae29067a62f1cc988482b7b4640fc12f20c
Saisha Sharma <saishasharma296@gmail.com>
2025-02-08 11:57:13 -0500

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,7 @@
+print("Circumference of a circle. The formula is 2*pi*r. Let's calculate it.")
+def mul(t,p,r):
+    return t*p*r
+t=2
+p=3.14
+r=int(input("Enter the radius of the circle which circumference you would like to calculate: "))
+print("The circumference of the circle is",mul(t,p,r))
