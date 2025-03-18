73afdedaa5b67cfccffc3055b7e1465b7aef1e4a
Saisha Sharma <saishasharma296@gmail.com>
2025-03-18 19:40:17 -0400

    Add files via upload

---
+++ country code.py
@@ -0,0 +1,14 @@
+country_code={
+    'India':'0091',
+    'USA':'001',
+    'UK':'044',
+    'Australia':'061',
+}
+print("Country code of India-")
+print(country_code.get('India', 'Not Found'))
+print("Country code of USA-")
+print(country_code.get('USA', 'Not Found'))
+print("Country code of UK-")
+print(country_code.get('UK', 'Not Found'))
+print("Country code of Japan-")
+print(country_code.get('Japan', 'Not Found'))
