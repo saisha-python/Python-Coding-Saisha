42d26da014c74059d7e513c66b6917c45d3c688b
Saisha Sharma <saishasharma296@gmail.com>
2025-04-06 12:11:15 -0400

    Add files via upload

---
+++ dog breed.py
@@ -0,0 +1,76 @@
+# Hello Ms. Vireetha! This code I wrote is a little different than the orginal assignment.
+# However, it still carries out the basic requirements of the assignment.
+# I hope you like it!
+user=input("Enter your name: ")
+print("Hello",user,"!")
+print("Welcome to the Sharma Family Animal Sanctuary, where we love and care all animals!")
+print("We specialize in caring for different types of endangered felines.")
+print("We have a variety of animals, including lions, tigers, leopards, and cheetahs.")
+print("You can learn more about each animal and even adopt one if you like!")
+class Lion:
+    species="feline"
+    def __init__(self, name, age, weight, endangered_status):
+        self.name=name
+        self.age=age
+        self.weight=weight
+        self.endangered_status=endangered_status
+Tatjana=Lion("Tatjana", 5, 300, "vulnerable")
+print("Animal #1 at the Sharma Family Animal Sanctuary's name is {}".format(Tatjana.name))
+print("Tatjana is a {}".format(Tatjana.species))
+print("Tatjana's age is {}".format(Tatjana.age))
+print("Tatjana's weight is {}".format(Tatjana.weight))
+print("Tatjana's endangered status is {}".format(Tatjana.endangered_status))
+class Iberian_Lynx:
+    species="feline"
+    def __init__(self, name, age, weight, endangered_status):
+        self.name=name
+        self.age=age
+        self.weight=weight
+        self.endangered_status=endangered_status
+Iberian_Lynx= Iberian_Lynx("Iberian Lynx", 3, 30, "endangered")
+print("Animal #2 at the Sharma Family Animal Sanctuary's name is {}".format(Iberian_Lynx.name))
+print("Iberian Lynx is a {}".format(Iberian_Lynx.species))
+print("Iberian Lynx's age is {}".format(Iberian_Lynx.age))
+print("Iberian Lynx's weight is {}".format(Iberian_Lynx.weight))
+print("Iberian Lynx's endangered status is {}".format(Iberian_Lynx.endangered_status))
+class Amur_Leopard:
+    species="feline"
+    def __init__(self, name, age, weight, endangered_status):
+        self.name=name
+        self.age=age
+        self.weight=weight
+        self.endangered_status=endangered_status
+Amur_Leopard= Amur_Leopard("Amur Leopard", 4, 90, "critically endangered")
+print("Animal #3 at the Sharma Family Animal Sanctuary's name is {}".format(Amur_Leopard.name))
+print("Amur Leopard is a {}".format(Amur_Leopard.species))
+print("Amur Leopard's age is {}".format(Amur_Leopard.age))
+print("Amur Leopard's weight is {}".format(Amur_Leopard.weight))
+print("Amur Leopard's endangered status is {}".format(Amur_Leopard.endangered_status))
+class Snow_Leopard:
+    species="feline"
+    def __init__(self, name, age, weight, endangered_status):
+        self.name=name
+        self.age=age
+        self.weight=weight
+        self.endangered_status=endangered_status
+Snow_Leopard= Snow_Leopard("Snow Leopard", 6, 120, "vulnerable")
+print("Animal #4 at the Sharma Family Animal Sanctuary's name is {}".format(Snow_Leopard.name))
+print("Snow Leopard is a {}".format(Snow_Leopard.species))
+print("Snow Leopard's age is {}".format(Snow_Leopard.age))
+print("Snow Leopard's weight is {}".format(Snow_Leopard.weight))
+print("Snow Leopard's endangered status is {}".format(Snow_Leopard.endangered_status))
+class Bengal_Tiger:
+    species="feline"
+    def __init__(self, name, age, weight, endangered_status):
+        self.name=name
+        self.age=age
+        self.weight=weight
+        self.endangered_status=endangered_status
+Bengal_Tiger= Bengal_Tiger("Bengal Tiger", 7, 450, "endangered")
+print("Animal #5 at the Sharma Family Animal Sanctuary's name is {}".format(Bengal_Tiger.name))
+print("Bengal Tiger is a {}".format(Bengal_Tiger.species))
+print("Bengal Tiger's age is {}".format(Bengal_Tiger.age))
+print("Bengal Tiger's weight is {}".format(Bengal_Tiger.weight))
+print("Bengal Tiger's endangered status is {}".format(Bengal_Tiger.endangered_status))
+print("Thank you for visiting the Sharma Family Animal Sanctuary!")
+print("We hope you enjoyed learning about our animals.")
