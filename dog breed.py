3cb82d609746cd397bcaa3c5b1d3ce361594239f
Saisha Sharma <saishasharma296@gmail.com>
2025-04-06 12:13:14 -0400

    Add files via upload

--- dog breed.py
+++ dog breed.py
@@ -1,12 +1,22 @@
 # Hello Ms. Vireetha! This code I wrote is a little different than the orginal assignment.
-# However, it still carries out the basic requirements of the assignment.
-# I hope you like it!
+# However, it still meets the basic requirements of the assignment.
+# I added a little bit of my own creativity to it, and I hope you like it!
+# I also did deep research on the animals I included in this code, to make sure I got the facts right.
+# Enjoy!
+
+# This code is was produced by $hadow$layer Productions, who also produced the T!p$ & TR!cK$ application, and the N0t3b00k application.
+# ©$hadow$layer Productions Applications- April 6, 2025, 12:10 PM EDT, 1128 Cain Court, Montgomery Township, NJ, USA, North America.
+# This code is licensed to $hadow$layer Productions, and cannot be used by any other individual or organization.
+# All rights reserved.
+
 user=input("Enter your name: ")
 print("Hello",user,"!")
 print("Welcome to the Sharma Family Animal Sanctuary, where we love and care all animals!")
 print("We specialize in caring for different types of endangered felines.")
 print("We have a variety of animals, including lions, tigers, leopards, and cheetahs.")
 print("You can learn more about each animal and even adopt one if you like!")
+
+
 class Lion:
     species="feline"
     def __init__(self, name, age, weight, endangered_status):
@@ -14,12 +24,15 @@ class Lion:
         self.age=age
         self.weight=weight
         self.endangered_status=endangered_status
+
 Tatjana=Lion("Tatjana", 5, 300, "vulnerable")
 print("Animal #1 at the Sharma Family Animal Sanctuary's name is {}".format(Tatjana.name))
 print("Tatjana is a {}".format(Tatjana.species))
 print("Tatjana's age is {}".format(Tatjana.age))
 print("Tatjana's weight is {}".format(Tatjana.weight))
 print("Tatjana's endangered status is {}".format(Tatjana.endangered_status))
+
+
 class Iberian_Lynx:
     species="feline"
     def __init__(self, name, age, weight, endangered_status):
@@ -27,12 +40,15 @@ class Iberian_Lynx:
         self.age=age
         self.weight=weight
         self.endangered_status=endangered_status
+
 Iberian_Lynx= Iberian_Lynx("Iberian Lynx", 3, 30, "endangered")
 print("Animal #2 at the Sharma Family Animal Sanctuary's name is {}".format(Iberian_Lynx.name))
 print("Iberian Lynx is a {}".format(Iberian_Lynx.species))
 print("Iberian Lynx's age is {}".format(Iberian_Lynx.age))
 print("Iberian Lynx's weight is {}".format(Iberian_Lynx.weight))
 print("Iberian Lynx's endangered status is {}".format(Iberian_Lynx.endangered_status))
+
+
 class Amur_Leopard:
     species="feline"
     def __init__(self, name, age, weight, endangered_status):
@@ -40,12 +56,15 @@ class Amur_Leopard:
         self.age=age
         self.weight=weight
         self.endangered_status=endangered_status
+
 Amur_Leopard= Amur_Leopard("Amur Leopard", 4, 90, "critically endangered")
 print("Animal #3 at the Sharma Family Animal Sanctuary's name is {}".format(Amur_Leopard.name))
 print("Amur Leopard is a {}".format(Amur_Leopard.species))
 print("Amur Leopard's age is {}".format(Amur_Leopard.age))
 print("Amur Leopard's weight is {}".format(Amur_Leopard.weight))
 print("Amur Leopard's endangered status is {}".format(Amur_Leopard.endangered_status))
+
+
 class Snow_Leopard:
     species="feline"
     def __init__(self, name, age, weight, endangered_status):
@@ -53,12 +72,15 @@ class Snow_Leopard:
         self.age=age
         self.weight=weight
         self.endangered_status=endangered_status
+
 Snow_Leopard= Snow_Leopard("Snow Leopard", 6, 120, "vulnerable")
 print("Animal #4 at the Sharma Family Animal Sanctuary's name is {}".format(Snow_Leopard.name))
 print("Snow Leopard is a {}".format(Snow_Leopard.species))
 print("Snow Leopard's age is {}".format(Snow_Leopard.age))
 print("Snow Leopard's weight is {}".format(Snow_Leopard.weight))
 print("Snow Leopard's endangered status is {}".format(Snow_Leopard.endangered_status))
+
+
 class Bengal_Tiger:
     species="feline"
     def __init__(self, name, age, weight, endangered_status):
@@ -66,11 +88,18 @@ class Bengal_Tiger:
         self.age=age
         self.weight=weight
         self.endangered_status=endangered_status
+
 Bengal_Tiger= Bengal_Tiger("Bengal Tiger", 7, 450, "endangered")
 print("Animal #5 at the Sharma Family Animal Sanctuary's name is {}".format(Bengal_Tiger.name))
 print("Bengal Tiger is a {}".format(Bengal_Tiger.species))
 print("Bengal Tiger's age is {}".format(Bengal_Tiger.age))
 print("Bengal Tiger's weight is {}".format(Bengal_Tiger.weight))
 print("Bengal Tiger's endangered status is {}".format(Bengal_Tiger.endangered_status))
+
+
 print("Thank you for visiting the Sharma Family Animal Sanctuary!")
 print("We hope you enjoyed learning about our animals.")
+print("If you would like to adopt one of our animals, let us know!")
+print("Please contact us at our offical email adress: saishasharma296@gmail.com")
+print("Until next time,",user)
+print("Goodbye!")
