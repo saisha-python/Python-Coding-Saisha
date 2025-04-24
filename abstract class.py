dd1d5b3b1f3bbe3f92e7ec7461e91bb639b96567
Saisha Sharma <saishasharma296@gmail.com>
2025-04-24 17:43:47 -0400

    Add files via upload

---
+++ abstract class.py
@@ -0,0 +1,14 @@
+#Abstract Class
+from abc import ABC, abstractmethod
+class Absclass(ABC):
+    def print(self, x):
+        print("The Passed Value:", x)
+    @abstractmethod
+    def task(self):
+        print("We're inside Absclass task")
+class test_class(Absclass):
+    def task(self):
+        print("We're inside test_class task")
+test_obj= test_class()
+test_obj.task
+test_obj.print(100)
